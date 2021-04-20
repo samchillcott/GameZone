1c154e0fa62c83a323ab49678edf0bed3ac9eb2e
Sam Chillcott <samchillcott@gmail.com>
2021-04-20 09:32:32 +0100

    Init page setup

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  console.log("test");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
