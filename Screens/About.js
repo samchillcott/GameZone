d7a56f8510527ae2235320c7c3dcc09e163c714b
Sam Chillcott <samchillcott@gmail.com>
2021-04-20 12:11:01 +0100

    add About Home and Review Details pages

--- Screens/About.js
+++ Screens/About.js
@@ -1,1 +1,14 @@
- 
+ import React from 'react'
+import { Text, View } from 'react-native'
+import {globalStyles} from './styles/global'
+
+
+const About = () => {
+    return (
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>About Screen</Text>
+        </View>
+    )
+}
+
+export default About
---
+++ Screens/ReviewDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import {globalStyles} from './styles/global'
+
+const ReviewDetails = () => {
+    return (
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>Review Details Screen
+            </Text>
+        </View>
+    )
+}
+
+export default ReviewDetails
