d66ff907c995b79351b247eda499622dec2b0a3e
Sam Chillcott <samchillcott@gmail.com>
2021-04-21 09:32:39 +0100

    pass params

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import { Text, View, Button } from 'react-native'
+import React, { useState } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler';
 import { globalStyles } from './styles/global'
 
 const Home = ({ navigation }) => {
 
+    const [reviews, setReviews] = useState([
+    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+    { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+    { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+  ]);
+
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button
-                title="go to Review Details"
-                onPress={() => navigation.navigate('ReviewDetails')}
+            <FlatList
+                data={reviews}
+                renderItem={({item}) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
             />
         </View>
     )
--- Screens/ReviewDetails.js
+++ Screens/ReviewDetails.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import { globalStyles } from './styles/global'
 
-const ReviewDetails = () => {
+const ReviewDetails = ({ route, navigation }) => {
+    const {title, body, rating} = route.params
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Review Details Screen
-            </Text>
+            <Text>{title}</Text>
+            <Text>{body}</Text>
+            <Text>{rating}</Text>
         </View>
     )
 }
