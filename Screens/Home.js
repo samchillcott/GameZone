0f7e02407dd9eb71afc09f08e02b0b146964c84e
Sam Chillcott <samchillcott@gmail.com>
2021-04-21 08:39:20 +0100

    inline navigation handler

--- Screens/Home.js
+++ Screens/Home.js
@@ -3,19 +3,13 @@ import { Text, View, Button } from 'react-native'
 import { globalStyles } from './styles/global'
 
 const Home = ({ navigation }) => {
-       console.log("fired");
-
-    const pressHandler = () => {
-       console.log("fired on press");
-        navigation.navigate('ReviewDetails')
-    }
 
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
             <Button
                 title="go to Review Details"
-                onPress={pressHandler}
+                onPress={() => navigation.navigate('ReviewDetails')}
             />
         </View>
     )
